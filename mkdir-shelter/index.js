bac3d6a2f0f78fe442c92485aa7c3e30722d80e1
popovaolha <fc.odessa.10@gmail.com>
2023-04-08 15:44:49 +0300

    feat: added functional of burger menu

--- mkdir-shelter/index.js
+++ mkdir-shelter/index.js
@@ -1,16 +1,31 @@
-"use strict"
+'use strict'
 
 console.log("Страница(main):\nВерстка страницы валидная(+4);\nЛоготип в хедере состоит из текстовых элементов (+1);\nСтраница содержит ровно один элемент <h1> (+1);\nдобавлен favicon +1;\nВёрстка соответствует макету (+35);\nТребования к css выполнены(+6);\nИнтерактивность элементов выполнена(+12);\nСтраница  Pets:\nВерстка страницы валидная(+4);\nЛоготип в хедере состоит из текстовых элементов (+1);\nСтраница содержит ровно один элемент <h1> (+1);\nдобавлен favicon +1;\nВёрстка соответствует макету (+15);\nТребования к css выполнены(+4);\nИнтерактивность элементов выполнена(+14);")
 
 document.addEventListener("click", showList);
 
+document.addEventListener('click', changeWidth);
+
+const BODY_MENU =  document.querySelector('.menu_body');
+
 function showList(e){
     const targetItem = e.target;
 
     if (targetItem.closest('.icon-menu')) {
-        document.documentElement.classList.toggle('menu-open');
-    }
+        document.documentElement.classList.toggle('menu-open');    
+}
 }
 
+function changeWidth(e){
+    const el = e.target;
+    console.log(el);
+    
+    if (el.closest('.menu_body') && window.innerWidth === 320) { 
+        BODY_MENU.style.width = '250px';
+       
+    } else {
+        BODY_MENU.style.width = '100%';
+    }
+ }
 
 
--- mkdir-shelter/pets.js
+++ mkdir-shelter/pets.js
@@ -2,10 +2,26 @@
 
 document.addEventListener("click", showList);
 
+document.addEventListener('click', changeWidth);
+
+const BODY_MENU =  document.querySelector('.menu_body');
+
 function showList(e){
     const targetItem = e.target;
 
     if (targetItem.closest('.pets-menu')) {
-        document.documentElement.classList.toggle('menu-open');
-    }
+        document.documentElement.classList.toggle('menu-open');    
 }
+}
+
+function changeWidth(e){
+    const el = e.target;
+    console.log(el);
+    
+    if (el.closest('.menu_body') && window.innerWidth === 320 ) { 
+        BODY_MENU.style.width = '250px';
+       
+    } else {
+        BODY_MENU.style.width = '100%';
+    }
+ }
