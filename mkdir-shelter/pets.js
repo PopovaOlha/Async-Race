c78a4e15215e97db693346f2251ba8910067127b
popovaolha <fc.odessa.10@gmail.com>
2023-04-08 18:04:48 +0300

    fix: create functional for infinity slider-caurusel

--- mkdir-shelter/pets.js
+++ mkdir-shelter/pets.js
@@ -18,8 +18,8 @@ function changeWidth(e){
     const el = e.target;
     console.log(el);
     
-    if (el.closest('.menu_body') && window.innerWidth === 320 ) { 
-        BODY_MENU.style.width = '250px';
+    if (el.closest('.menu_body') && window.innerWidth >= 320 && window.innerWidth <= 768 ) { 
+        BODY_MENU.style.width = '80%';
        
     } else {
         BODY_MENU.style.width = '100%';
