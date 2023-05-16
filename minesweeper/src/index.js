2a12b0c14394b7833f6d8244684d041824756388
PopovaOlha <fc.odessa.10@gmail.com>
2023-05-16 14:26:31 +0300

    feat: add a style from scss

--- minesweeper/src/index.js
+++ minesweeper/src/index.js
@@ -1,14 +1,5 @@
 import modulName from './module-1';
-
 import '../scss/common.scss';
-import codeImageUrl from "./images/stopwatch.png";
-import flagUrl from './images/flag_ok.png';
 
-const codeImage = document.createElement('img');
-codeImage.classList.add('images')
-codeImage.src = codeImageUrl;
 
-const codeFlag = document.createElement('img');
-codeFlag.classList.add('images')
-codeFlag.src= flagUrl;
-document.body.append(codeImage, codeFlag);
+
--- minesweeper/src/module-1.js
+++ minesweeper/src/module-1.js
@@ -1,1 +1,9 @@
 export default 'module-1';
+import stopwatchUrl from "./images/stopwatch.png";
+import flagUrl from './images/flag_ok.png';
+import bombUrl from './images/Bomb-PNG-Free-Download.png';
+
+
+
+
+
