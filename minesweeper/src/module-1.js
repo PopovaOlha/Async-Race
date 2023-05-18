9e4e3a1d5399f4f02a1568ae8060fa3d55f4b000
PopovaOlha <fc.odessa.10@gmail.com>
2023-05-18 18:39:03 +0300

    refactor: add a recursion when the square is empty

--- minesweeper/src/module-1.js
+++ minesweeper/src/module-1.js
@@ -7,8 +7,8 @@ bomb.src = bombUrl;
 const WIDTH = 10;
 const HEIGHT = 10;
 const BOMB_AMOUNT = 10;
-const SQUERES = [];
-const isGameOver = false;
+const SQUARES = [];
+const IS_GAME_OVER = false;
 
 document.addEventListener('DOMContentLoaded', () => {
  const grid = document.querySelector('.grid');
@@ -24,38 +24,39 @@ const createBoard = function() {
         square.setAttribute('id', i);
         square.classList.add(shuffledArray[i]);
         grid.appendChild(square);
-        SQUERES.push(square);
+        SQUARES.push(square);
 
         square.addEventListener('click', (e) => {
             click(square);
         })
     }
-    for (let i = 0; i , SQUERES.length; i++) {
+    for (let i = 0; i , SQUARES.length; i++) {
         let total = 0;
         const leftEdge = (i % WIDTH === 0);
         const rightEdge = (i % WIDTH === WIDTH -1);
 
-        if (SQUERES[i].classList.contains('valid')) {
-            if (i > 0 && !leftEdge && SQUERES[i - 1].classList.contains('bomb')) total ++
-            if (i > 9 && !rightEdge && SQUERES[i + 1 -WIDTH].classList.contains('bomb')) total ++
-            if (i > 10 && SQUERES[i - WIDTH].classList.contains('bomb')) total ++
-            if (i > 11 && !leftEdge && SQUERES[i - 1 -WIDTH].classList.contains('bomb')) total ++
-            if (i < 98 && !rightEdge && SQUERES[i + 1].classList.contains('bomb')) total ++
-            if (i < 90 && !leftEdge && SQUERES[i - 1 +WIDTH].classList.contains('bomb')) total ++
-            if (i < 88 && !rightEdge && SQUERES[i + 1 +WIDTH].classList.contains('bomb')) total ++
-            if (i < 89 && SQUERES[i + WIDTH].classList.contains('bomb')) total ++
-            SQUERES[i].setAttribute('data', total);
-            console.log(SQUERES[i])
+        if (SQUARES[i].classList.contains('valid')) {
+            if (i > 0 && !leftEdge && SQUARES[i - 1].classList.contains('bomb')) total ++
+            if (i > 9 && !rightEdge && SQUARES[i + 1 -WIDTH].classList.contains('bomb')) total ++
+            if (i > 10 && SQUARES[i - WIDTH].classList.contains('bomb')) total ++
+            if (i > 11 && !leftEdge && SQUARES[i - 1 -WIDTH].classList.contains('bomb')) total ++
+            if (i < 98 && !rightEdge && SQUARES[i + 1].classList.contains('bomb')) total ++
+            if (i < 90 && !leftEdge && SQUARES[i - 1 +WIDTH].classList.contains('bomb')) total ++
+            if (i < 88 && !rightEdge && SQUARES[i + 1 +WIDTH].classList.contains('bomb')) total ++
+            if (i < 89 && SQUARES[i + WIDTH].classList.contains('bomb')) total ++
+            SQUARES[i].setAttribute('data', total);
+            console.log(SQUARES[i])
         }
     }
  }
 createBoard()
 
 function click(square) {
-    if (isGameOver) return
+    let currentId = square.id;
+    if (IS_GAME_OVER) return
     if (square.classList.contains('checked') || square.classList.contains('flag')) return
     if (square.classList.contains('bomb')) {
-        alert('game over');
+        gameOver(square);
     } else {
         let total = square.getAttribute('data');
         if (total != 0) {
@@ -63,9 +64,65 @@ function click(square) {
             square.innerHTML = total;
             return
         }
-        square.classList.add('checked')
+        checkSquare(square, currentId);
+        
     }
+    square.classList.add('checked')
 }
+
+function checkSquare(square, currentId) {
+    const isLeftEdge = (currentId % WIDTH === 0);
+    const isRightEdge = (currentId % WIDTH === WIDTH -1);
+
+    setTimeout(() => {
+        if (currentId > 0 && !isLeftEdge) {
+            const newId = SQUARES[parseInt(currentId) -1].id
+            const newSquare = document.getElementById(newId)
+            click(newSquare);
+        }
+        if (currentId > 9 && !isRightEdge) {
+            const newId = SQUARES[parseInt(currentId) + 1 -WIDTH].id
+            const newSquare = document.getElementById(newId);
+            click(newSquare);
+        }
+        if (currentId > 10) {
+            const newId = SQUARES[parseInt(currentId -WIDTH)].id
+            const newSquare = document.getElementById(newId);
+            click(newSquare);
+        }
+        if (currentId > 11 && !isLeftEdge) {
+            const newId = SQUARES[parseInt(currentId) -1 -WIDTH].id
+            const newSquare = document.getElementById(newId);
+            click(newSquare);
+        }
+        if (currentId < 98 && !isRightEdge) {
+            const newId = SQUARES[parseInt(currentId) +1].id
+            const newSquare = document.getElementById(newId);
+            click(newSquare);
+        }
+        if (currentId < 90 && !isLeftEdge) {
+            const newId = SQUARES[parseInt(currentId) -1 +WIDTH].id
+            const newSquare = document.getElementById(newId);
+            click(newSquare);
+        }
+        if (currentId < 88 && !isRightEdge) {
+            const newId = SQUARES[parseInt(currentId) +1 +WIDTH].id
+            const newSquare = document.getElementById(newId);
+            click(newSquare);
+        }
+        if (currentId < 89) {
+            const newId = SQUARES[parseInt(currentId) +WIDTH].id
+            const newSquare = document.getElementById(newId);
+            click(newSquare);
+        }
+    },10)
+}
+
+
+
+
+
+
 
 })
 
