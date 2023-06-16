fff7924bf8c2fa8df5dd1a9f7ae50634f25b7a90
PopovaOlha <fc.odessa.10@gmail.com>
2023-06-16 22:21:11 +0300

    init: start work with rs-css project

---
+++ rs-css/webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const EslintPlugin = require('eslint-webpack-plugin');
+
+module.exports = {
+    entry: path.resolve(__dirname, './src/index.ts'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'my-bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            { test: /\.ts$/i, use: 'ts-loader' },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+            {},
+        ],
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    plugins: [new HtmlWebpackPlugin({ template: './src/index.html' }), new EslintPlugin({ extensions: 'ts' })],
+    devServer: {
+        port: 4444,
+        open: true,
+    },
+};
