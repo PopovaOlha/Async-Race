21f3377110206cc9454e2a3fbd2b53b6eb6c0f4b
PopovaOlha <fc.odessa.10@gmail.com>
2023-06-18 12:49:47 +0300

    feat: add a new folder with pictures

--- rs-css/webpack.config.js
+++ rs-css/webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const EslintPlugin = require('eslint-webpack-plugin');
 
 module.exports = {
-    entry: path.resolve(__dirname, './src/index.ts'),
+    entry: path.resolve(__dirname, './src/index'),
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'my-bundle.js',
